7d3d6ac6210dd5bbe395a5e851fc692064321fda
Ryan Upton <ryanjupton@gmail.com>
2011-07-21 21:08:00 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeetGeekHaiku::Application.config.secret_token = '5ff7579be2417d1884bef15906a840d28b715abfb6c4284bb984e4c2870b62c2766d716d1b1821b094b2b7efdf8f6445dfed8d297fdaee1accc97ee25709d489'
